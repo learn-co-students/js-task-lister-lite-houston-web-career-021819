28934f882c5ea40f0df92381211a11689de38d64
github username <github email address>
2019-04-02 17:52:10 -0500

    all done yay

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+// grab the elements
+const entireTaskForm = document.getElementById("create-task-form")
+const descriptionBox = document.getElementById("new-task-description")
+const taskList = document.getElementById("tasks")
+
+// set listener
+entireTaskForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    const newTask = document.createElement("li")
+    newTask.append(descriptionBox.value)
+    taskList.appendChild(newTask)
+})
+
+
+
+// function that invokes w/ listener
+// const createTask
+
+// const tasks = []
+
+// add listener to the entire object
+// document.getElementById("create-task-form").addEventListener("submit", function(e)){
+// be more specific
+// document.getElementById("new-task-description")
+
+
+
 });
