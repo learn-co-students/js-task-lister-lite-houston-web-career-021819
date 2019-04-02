aba2ce6532fd18f98883189d07818d72ba696ae6
github username <github email address>
2019-04-02 17:31:14 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let inputbox = document.querySelector("#new-task-description");
+  let tasklist = document.querySelector("#tasks");
+  let submitButton = document.forms[0].querySelectorAll("input")[1];
+
+  submitButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    //new task is added
+    let newTask = document.createElement("li");
+    newTask.innerText = inputbox.value;
+    tasklist.appendChild(newTask);
+    inputbox.value = ("");
+
+    //X button is created
+    let xbutton = document.createElement("BUTTON");
+    xbutton.innerHTML = "x";
+    newTask.append(xbutton);
+    
+    xbutton.addEventListener("click", function(e){
+      newTask.remove();
+    })
+  })
 });
