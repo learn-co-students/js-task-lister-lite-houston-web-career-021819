3934312359186073499b404ccee9c6758fb89e79
JingChen <jingchenjc2019@gmail.com>
2019-04-02 17:48:42 -0500

    delete button added for each task.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let input = document.querySelector("#new-task-description");
-  let submit = document.querySelector("#create-task-form").querySelector('input[type="submit"]')
+  let submit = document.querySelector("#create-task-form").querySelector('input[type="submit"]');
+
+  
   if (input.value !== null){
     if (submit.addEventListener('click', function(){
       event.preventDefault()
@@ -12,6 +16,20 @@ document.addEventListener("DOMContentLoaded", () => {
         li.innerHTML = `<strong>${input.value}</strong>`; //add some HTML style
         ul.append(li);
       };
+    
+      //create delete button, assign text to button
+      let deleteButton = document.createElement("button");
+      deleteButton.innerText = "x";
+      //append button to li element
+      li.append(deleteButton);
+      //add button event
+      deleteButton.addEventListener('click', function(){
+        event.preventDefault();
+        //remove the first <li> element from ul list.
+        //since button only works for each li, so the li is the first element in the ul list.
+        ul.removeChild(ul.childNodes[0]);
+      })
     }));
+    
   }
 })
