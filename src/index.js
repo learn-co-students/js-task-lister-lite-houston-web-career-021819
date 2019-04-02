a51300aa0569a85c416efef278cfb53ce13cdf0a
JingChen <jingchenjc2019@gmail.com>
2019-04-02 16:59:48 -0500

    Done with basic function.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let input = document.querySelector("#new-task-description");
+  let submit = document.querySelector("#create-task-form").querySelector('input[type="submit"]')
+  if (input.value !== null){
+    if (submit.addEventListener('click', function(){
+      event.preventDefault()
+      let ul = document.querySelector("#list").querySelector('ul');
+      let li = document.createElement('li');
+      li.innerText = input.value;
+      ul.append(li);
+    }));
+  }
+})
